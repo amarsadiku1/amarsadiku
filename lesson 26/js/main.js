59bab58f3ba8eac4e04cb880bf485faf14a2bad4
amarsadiku1 <amarsadiku79@gmail.com>
2025-09-01 17:50:26 +0200

    lesson 26

---
+++ lesson 26/js/main.js
@@ -0,0 +1,57 @@
+console.log("welcome");
+var x=5;  
+
+console.log(x);
+
+
+var x1=10;
+var x2=5;
+console.log('the resulte'+(x1-x2));
+
+
+
+var n=99;
+console.log(n);
+ 
+var n =99;
+n--;
+console.log(n);
+var n2=99;
+n2++;
+console.log(n2);
+
+var q=18;
+var p=q**2;
+console.log(p+q)
+
+
+var z2=15;  
+z2 +=5;
+console.log(z2)
+ 
+var z3=15;  
+z3 +=5;
+console.log(z3)
+
+var z=15;  
+z +=5;
+console.log(z)
+
+var z4=15;  
+z4 +=5;
+console.log(z4)
+
+var z5=15;  
+z5 +=5;
+console.log(z5)
+
+
+
+
+var y1=5;
+var y2=10;
+console.log(y1==y2)
+console.log(y1>y2)
+console.log(y1<y2)
+console.log(y1!=y2)
+console.log(y1===y2)
