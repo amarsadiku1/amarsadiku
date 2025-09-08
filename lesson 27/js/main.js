271f482975c880d0c90cf13f806e9c3f9878ccaf
amarsadiku1 <amarsadiku79@gmail.com>
2025-09-08 17:42:45 +0200

    lesson 27

---
+++ lesson 27/js/main.js
@@ -0,0 +1,46 @@
+function sum(number1, number2){
+    return number1 + number2 ;
+}
+console.log(sum(3,4)) ;
+
+var name = "Digital school";{
+    console.log(sum(3,4));
+  }
+
+function dsfunction() {
+    var localVar = " digital school";
+
+}
+
+
+function trangel (gj,l) {
+    o,s *gj* l
+} 
+console,kiy(trangel(5,4)) 
+
+
+
+var car={
+    name:"mercedes",
+    color: "red",
+    year:2020 
+     
+startEngine : function() {
+    alert("Vrooommmm!!!");
+}
+
+get getName() {
+    return this.name;
+}
+set setName(name) {
+    this.name=name;
+}
+}
+
+console.log(car.color);
+console.log( car.name);
+car.startEngine();
+
+console.log(car.getName);
+car.setName="BMW";
+console.log(car.getName);
